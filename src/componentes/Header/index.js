ae31684c1d23268ef4136cca940ee732eae7db09
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2024-08-06 08:27:01 -0300

    Iniciando Header

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header center">
+        <picture>
+          <img src='./assets/banner2.jpg'></img>
+        </picture>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Produtos</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
